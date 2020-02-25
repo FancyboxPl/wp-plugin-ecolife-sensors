e1c62dd8eaf295625e74982188272609b26cbb3b
Marcin Kosmala <kosmala007@gmail.com>
2020-02-25 09:49:08 +0100

    Change urls in buttons

--- templates/map.template.php
+++ templates/map.template.php
@@ -7,10 +7,10 @@
     <h2 class="ecolife-sensors__header">Dostęp do platformy z wielu źródeł</h2>
     <p class="ecolife-sensors__p">Dane uzyskane z pomiarów są na bieżąco przesyłane, a następnie gromadzone w chmurze, skąd udostępniane są Tobie zawsze i wszędzie, kiedy tylko chcesz i niezależnie, gdzie jesteś. Możesz kontrolować stan powietrza w Twoim mieszkaniu i w pracy, w przedszkolu lub w szkole, gdzie przebywają Twoje dzieci, w swoim mieście oraz w okolicy </p>
     <div class="ecolife-sensors__btns">
-      <a href="https://play.google.com/store/apps/details?id=com.eu.ecolife.healthy_breathing" class="ecolife-sensors__btn" target="blank" rel="nofollow noopener">
+      <a href="https://itunes.apple.com/pl/app/ecolife-healthy-breathing/id1238331106" class="ecolife-sensors__btn" target="blank" rel="nofollow noopener">
         <?php echo file_get_contents($pluginDir.'assets/svg/icon-app-store.svg') ?> <span>App Store</span>
       </a>
-      <a href="https://itunes.apple.com/pl/app/ecolife-healthy-breathing/id1238331106" class="ecolife-sensors__btn" target="blank" rel="nofollow noopener">
+      <a href="https://play.google.com/store/apps/details?id=com.eu.ecolife.healthy_breathing" class="ecolife-sensors__btn" target="blank" rel="nofollow noopener">
         <?php echo file_get_contents($pluginDir.'assets/svg/icon-google-play.svg') ?> <span>Google Play</span>
       </a>
       <a href="http://app.ecolife.eu.com/" class="ecolife-sensors__btn" target="blank" rel="nofollow noopener">
