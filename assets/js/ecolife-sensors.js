400dd8c3f4ace6a09614f02aaecbd972372c0696
Marcin Kosmala <kosmala007@gmail.com>
2020-02-25 09:24:43 +0100

    Create map

---
+++ assets/js/ecolife-sensors.js
@@ -0,0 +1,77 @@
+class EcolifeSensors {
+  constructor() {
+    this.isLoaded = false;
+    this.mapElem = document.querySelector('#ecolife-sensors-map');
+    this.token = 'pk.eyJ1IjoiZmFuY3lib3hwbCIsImEiOiJjazcwZzFwdzgwOWxkM2xtcGRudWN3bDlpIn0.F_9R6_YuG0Nej3RZVCwc1g';
+    this.observer = null;
+
+    if (this.mapElem) {
+      this.observerMap();
+    }
+  }
+
+  initMap() {
+    L.mapbox.accessToken = this.token;
+    const map = L.mapbox.map(this.mapElem, 'mapbox.streets')
+    .setView([52.069442, 19.480311], 7);
+
+    map.scrollWheelZoom.disable();
+
+    const featureLayer = L.mapbox.featureLayer()
+      .loadURL('https://dev.ecolife.eu.com:58200/EcoLife/MeasurePoint/publicDevices')
+      .on('layeradd', function (e) {
+        const marker = e.layer;
+        const icon = marker.feature.properties.icon;
+        icon.iconUrl = 'https://www.ecolife.eu.com' + icon.iconUrl;
+        marker.setIcon(L.icon(icon));
+      })
+      .addTo(map);
+
+    map.fitBounds([
+      [54.288776, 23.166448],
+      [50.409003, 16.302454],
+    ], {
+      padding: [10, 10]
+    });
+  }
+
+  observerMap() {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          this.lazyLoadMap();
+        }
+      });
+    });
+
+    observer.observe(this.mapElem);
+  }
+
+  lazyLoadMap() {
+    if (!this.isLoaded) {
+      const script = document.createElement('script');
+      script.src = 'https://api.mapbox.com/mapbox.js/v3.2.1/mapbox.js';
+      script.onload = () => {
+        this.loadStyles('https://api.mapbox.com/mapbox.js/v3.2.1/mapbox.css')
+        this.initMap();
+      };
+      document.getElementsByTagName('head')[0].appendChild(script);
+    }
+
+    this.isLoaded = true;
+  }
+
+  loadStyles(path, preload = false) {
+    const head = document.getElementsByTagName('head')[0];
+    const link = document.createElement('link');
+    link.href = path;
+    link.type = 'text/css';
+    link.rel = `stylesheet${ preload ? ` preload` : `` }`;
+    link.as = 'style';
+    head.appendChild(link);
+  }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  new EcolifeSensors();
+});
--- ecolife-sensors.php
+++ ecolife-sensors.php
@@ -9,19 +9,23 @@
 **/
 
 add_shortcode('ecolife-sensors-map', function () {
-    return file_get_contents(dirname(__FILE__).'/templates/map.html');
+    ob_start();
+    include(dirname(__FILE__).'/templates/map.template.php');
+    $content = ob_get_clean();
+
+    return $content;
 });
 
 add_action('wp_enqueue_scripts', function () {
     global $post;
     if (has_shortcode( $post->post_content, 'ecolife-sensors-map')) {
-        wp_enqueue_style('ecolife-sensors-style', plugin_dir_url(__FILE__).'assets/ecolife-sensors.css');
+        wp_enqueue_style('ecolife-sensors-style', plugin_dir_url(__FILE__).'assets/css/ecolife-sensors.css');
     }
 });
 
 add_action('wp_footer', function () {
     global $post;
     if (has_shortcode( $post->post_content, 'ecolife-sensors-map')) {
-        wp_enqueue_script('ecolife-sensors-script', plugin_dir_url(__FILE__).'assets/ecolife-sensors.js');
+        wp_enqueue_script('ecolife-sensors-script', plugin_dir_url(__FILE__).'assets/js/ecolife-sensors.js');
     }
 });
